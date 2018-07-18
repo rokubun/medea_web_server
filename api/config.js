8952ffc977bd866a791d5b369d43434bcfb2e04c
Jenkins - Oompaloompa <jenkins@rokubun.cat>
2018-07-18 12:59:34 +0200

    Build: 67

--- api/config.js
+++ api/config.js
@@ -8,7 +8,7 @@ const FRONT_PORT = process.env.FRONT_PORT || 80;
 // * Note : This configuration points to RTKrcv
 const confNet = {
   port: process.env.TCP_PORT || 50,
-  host: process.env.HOST || '192.168.1.112'
+  host: process.env.HOST || '127.0.0.1:3000'
 }
 
 
