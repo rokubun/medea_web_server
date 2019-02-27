16ae5a1d98bdba5b245b6466c9e0ad8429d92517
jesse <jesse.ramon@rokubun.cat>
2019-02-27 14:51:03 +0000

    Add start cellular connection in start_sara.sh

--- scripts/start_sara.sh
+++ scripts/start_sara.sh
@@ -4,7 +4,15 @@
 # Turn SARA on
 source ~/xgpio_functions.sh
 xgpio_high 7
-sleep 10
+sleep 30
+
+# Takes the saved pin
+pin=$(nmcli --terse -s --fields gsm con show cellular | grep -P "(?<=pin:)[1-90-9]*" | awk -F: '{print $2}')
+
+if [ $pin != "--" ]; then
+  modem_idx=$(bash /root/medea_web_server/scripts/check_modemidx.sh)
+  mmcli -i "$modem_idx" --pin="$pin"
+fi
 
 # Ofonod is already running in background on startup
 # python3 $OFONO_SCRIPTS/enter-pin "pin" "3782"
